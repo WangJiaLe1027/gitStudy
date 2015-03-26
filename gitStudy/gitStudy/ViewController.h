e83671096c549f16e180ecdd6e507cdefe15094a
wangjiale <p96588@qq.com>
2015-03-26 14:35:04 +0800

    for github study

---
+++ gitStudy/gitStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitStudy
+//
+//  Created by wangjiale on 15/3/26.
+//  Copyright (c) 2015年 wangjiale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitStudy/gitStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitStudy
+//
+//  Created by wangjiale on 15/3/26.
+//  Copyright (c) 2015年 wangjiale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
